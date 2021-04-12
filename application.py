8a2d2932494cad08d72ca3b5f50d892b6758fa54
Kevin <36983509+kevinisback@users.noreply.github.com>
2021-04-13 00:16:09 +1000

    Update application.py

--- application.py
+++ application.py
@@ -106,7 +106,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>I am back, Cindy.</h1>
+    <h1>I am back, Cindy.12:16</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
